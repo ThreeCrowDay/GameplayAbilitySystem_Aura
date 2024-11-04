e81fcbbc60e5aebe77a1d3ba9e57a11aaeb0ba6d
ThreeCrowDay <105822920+ThreeCrowDay@users.noreply.github.com>
2024-11-04 17:58:40 -0500

    Enemy Interface

--- Source/Aura/Public/AuraEnemy.h
+++ Source/Aura/Public/AuraEnemy.h
@@ -4,14 +4,18 @@
 
 #include "CoreMinimal.h"
 #include "AuraCharacterBase.h"
+#include "TargetInterface.h"
 #include "AuraEnemy.generated.h"
 
 /**
  * 
  */
 UCLASS()
-class AURA_API AAuraEnemy : public AAuraCharacterBase
+class AURA_API AAuraEnemy : public AAuraCharacterBase, public ITargetInterface
 {
 	GENERATED_BODY()
+public:
+	virtual void HighlightActor() override;
+	virtual void UnHighlightActor() override;
 	
 };
